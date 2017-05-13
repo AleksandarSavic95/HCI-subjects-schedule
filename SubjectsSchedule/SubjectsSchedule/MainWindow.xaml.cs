de42fe31ef2ee1afe27978e56799eb89127026a9
Aleksandar Savić <alek_bn@hotmail.com>
2017-05-13 16:44:46 +0200

    Dodata podrška za Help, kao i komande za menije.

--- SubjectsSchedule/SubjectsSchedule/MainWindow.xaml.cs
+++ SubjectsSchedule/SubjectsSchedule/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,140 @@ namespace SubjectsSchedule
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private bool _enable;
+
+        public bool MenuEnabled
+        {
+            get
+            {
+                return _enable;
+            }
+            set
+            {
+                if (_enable != value)
+                {
+                    _enable = value;
+                    OnPropertyChanged("MenuEnabled");
+                }
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void HelloWorld_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void HelloWorld_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Hello world!");
+        }
+
+        private void Enable_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void Enable_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void Komanda_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Komanda!");
+        }
+
+        private void Komanda_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = MenuEnabled;
+        }
+
+        private void Ugradjene_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var w = new MyCommands.UgradjeneKomande();
+            w.ShowDialog();
+        }
+
+        private void Ugradjene_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            //var w = new Grafika.CustomRender();
+            //w.ShowDialog();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            //var w = new Table.TableExample();
+            //w.ShowDialog();
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            //var w = new Grafika3D.Kocka();
+            //w.ShowDialog();
+        }
+
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
+            //var w = new Stil.StilPrimer();
+            //w.ShowDialog();
+        }
+
+        private void MenuItem_Click_4(object sender, RoutedEventArgs e)
+        {
+            //var w = new DDrop.DragDropWindow();
+            //w.ShowDialog();
+        }
+
+        private void MenuItem_Click_5(object sender, RoutedEventArgs e)
+        {
+            //var w = new Kontrole.ToolbarTreeContext();
+            //w.ShowDialog();
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            IInputElement focusedControl = FocusManager.GetFocusedElement(Application.Current.Windows[0]);
+            // da radi za sve kontrole
+            // DependencyObject focusedControl = FocusManager.GetFocusScope(Application.Current.Windows[0]);
+            if (focusedControl is DependencyObject)
+            {
+                Console.WriteLine("focusedControl je DependencyObject");
+                string str = HelpProvider.GetHelpKey((DependencyObject)focusedControl);
+                HelpProvider.ShowHelp(str, this);
+            }
+        }
+
+        /// <summary>
+        /// Dodatak za help - sprega između C# i JavaScripta (?)
+        /// </summary>
+        /// <param name="param"></param>
+        public void doThings(string param)
+        {
+            //btnOK.Background = new SolidColorBrush(Color.FromRgb(32, 64, 128));
+            Title = param;
         }
     }
 }
---
+++ SubjectsSchedule/SubjectsSchedule/MyCommands/CustomCommands.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace SubjectsSchedule.MyCommands
+{
+    public static class CustomCommands
+    {
+        public static readonly ICommand testKomanda = new TestKomanda();
+    }
+
+    // da hocemo da radimo i sa precicama, trebalo bi da nasledimo RoutedUICommand, recimo
+    public class TestKomanda : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            //ovde ide model-level obrada komande,
+            //tj. dio koji ne radi sa interfejsom
+            Console.WriteLine("Test");
+            Console.Beep();
+        }
+    }
+}
