6228b714e5820d100c5bd880c3c782a5ddbef1f0
Aleksandar Savić <alek_bn@hotmail.com>
2017-05-28 12:28:24 +0200

    Test Calendar tipa WeekRange - za sad nije bolji od TimeTable-a.
    Dodat apointment u kalendar onLoad.

--- SubjectsSchedule/SubjectsSchedule/Schedules/ScheduleScheme.xaml.cs
+++ SubjectsSchedule/SubjectsSchedule/Schedules/ScheduleScheme.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MindFusion.Scheduling;
+using SubjectsSchedule.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -33,14 +35,41 @@ namespace SubjectsSchedule.Schedules
             // ne radi "Lt-sr-SP", pa mora hrvatski ;(
             //kalendar.Culture = new System.Globalization.CultureInfo("hr-HR");
 
-            /// Popunjavanje kalendara danima u sedmici <see cref="Model.ScheduleDays"/>
+            /// Popunjavanje kalendara danima u sedmici <see cref="ScheduleDays"/>
             for (int i = 0; i < 6; i++)
-            {
-                kalendar.TimetableSettings.Dates
-                    .Add(Model.ScheduleDays.workDays[i]);
-            }
+                kalendar.TimetableSettings.Dates.Add(ScheduleDays.workDays[i]);
+            
+            // Da na kalendaru pravimo termine tipa MyTermin, a ne Appointment
+            kalendar.InteractiveItemType = typeof(MyTermin);
 
             kalendar.EndInit();
+
+            // Serialization support
+            //Schedule.RegisterItemClass(typeof(MyTermin), "mytermin", 1);
+
+            // Create an appointment
+            Appointment app = new Appointment();
+            app.HeaderText = "Meet George";
+            app.DescriptionText = "This is a sample appointment";
+
+            /* *
+            app.StartTime = ScheduleDays.workDays[0].Add(new TimeSpan(9, 15, 0));
+            app.EndTime = app.StartTime.Add(new TimeSpan(0, 65, 0)); // */
+
+            /* */
+            app.StartTime = new DateTime(2017, 5, 25, 14, 0, 0);
+            app.EndTime = new DateTime(2017, 5, 25, 16, 30, 0); // */
+
+            kalendar.Schedule.Items.Add(app);
+        }
+
+        private void Calendar_ItemClick(object sender, MindFusion.Scheduling.Wpf.ItemMouseEventArgs e)
+        {
+            if (e.Item is MyTermin)
+            {
+                kalendar.ResetDrag();
+                MessageBox.Show("This is our item.");
+            }
         }
     }
 }
